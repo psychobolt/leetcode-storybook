dd2b96ca78bf24f114a98ef0c7de770382341a29
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-07-16 19:06:08 -0700

    Update solution and add explanation

--- src/DataStructure/Array/2D/FindDiagonalOrder/javascript/findDiagonalOrder.es3.cjs
+++ src/DataStructure/Array/2D/FindDiagonalOrder/javascript/findDiagonalOrder.es3.cjs
@@ -4,24 +4,6 @@
  * [498] Diagonal Traverse
  */
 
-/*
- solving mat = [[1,2,3],[4,5,6],[7,8,9]]
-k   i, j    o = out of bounds  notes
-0   0, 0 |      ,
-   -1, 1 | i-- o, j++
-    0, 1 | i++  ,
-1   1, 0 | i++  , j--
-2   2,-1 | i++  , j-- o
-    2, 0 |      , j++
-3   1, 1 | i--  , j++
-4   0, 2 | i--  , j++
-5   0, 3 | x    , j++ o         actual=-1,3
-6   1, 2 | i++  , j--
-7   2, 1 | i++  , j--
-8   3, 1 | i++ o,               actual=3,0
-9   2, 2 | i--  , j++           actual=4,0
-*/
-
 // @lc code=start
 /**
  * @param {number[][]} mat
@@ -31,8 +13,8 @@ function findDiagonalOrder(mat) {
   var m = mat.length;
   var n = mat[0].length;
   var result = [];
-  var i = 1;
-  var j = -1;
+  var i = 0;
+  var j = 0;
   var k;
   var a = -1;
   var b = 1;
@@ -48,37 +30,31 @@ function findDiagonalOrder(mat) {
     }
     return result;
   }
-  for (k = 0; k < m * n - 1; k += 1) {
+  result.push(mat[i][j]);
+  for (k = 1; k < m * n; k += 1) {
     i += a;
     j += b;
-    if (i < m && j === n) {
+    if (i === m) {
+      a *= -1;
+      b *= -1;
+      i -= 1;
+      j += 2;
+    } else if (j === n) {
+      a *= -1;
+      b *= -1;
       i += 2;
+      j -= 1;
+    } else if (i < 0) {
       a *= -1;
       b *= -1;
-      j += b;
-    }
-    if (i === m && j < n) {
+      i += 1;
+    } else if (j < 0) {
       a *= -1;
       b *= -1;
-      i += a;
-      j += b;
-      j += b;
+      j += 1;
     }
-    if (j < 0) {
-      a *= -1;
-      b *= -1;
-      j += b;
-    }
-    if (i < 0) {
-      a *= -1;
-      b *= -1;
-      i += a;
-    }
-    if ((0 <= i && i < m) && (0 <= j && j < n)) {
-      result.push(mat[i][j]);
-    }
+    result.push(mat[i][j]);
   }
-  result.push(mat[m - 1][n - 1]);
   return result;
 }
 // @lc code=end
