5fb076af86810ae4d41f3fc93d2ac301e2004a56
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-07-17 20:58:51 -0700

    Add problem 412

---
+++ src/DataStructure/Array/1D/FizzBuzz/javascript/fizzBuzz.es3.cjs
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=412 lang=javascript
+ *
+ * [412] Fizz Buzz
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+  var i;
+  var result = [];
+  for (i = 1; i <= n; i += 1) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      result.push('Fizz');
+    } else if (i % 5 === 0) {
+      result.push('Buzz');
+    } else {
+      result.push(i + '');
+    }
+  }
+  return result;
+};
+// @lc code=end
+
+module.exports = fizzBuzz;
