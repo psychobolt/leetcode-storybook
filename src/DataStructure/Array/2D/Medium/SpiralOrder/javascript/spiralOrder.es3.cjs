5c317251ed0a3a01c56fb40aab9e9f7396ac5b89
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-08-03 20:40:18 -0700

    Update solution

--- src/DataStructure/Array/2D/Medium/SpiralOrder/javascript/spiralOrder.es3.cjs
+++ src/DataStructure/Array/2D/Medium/SpiralOrder/javascript/spiralOrder.es3.cjs
@@ -11,38 +11,46 @@
  */
 var spiralOrder = function (matrix) {
   var result = [];
+  var m = matrix.length;
+  var n = matrix[0].length;
   var i = 0;
   var j = 0;
-  var a;
-  var b;
-  var m = matrix.length;
-  var n = matrix[i].length;
-  while (result.length < m * n) {
+  var x0 = 0;
+  var x1 = n - 1;
+  var y0 = 0;
+  var y1 = m - 1;
+  var length = m * n;
+  while (result.length < length) {
     result.push(matrix[i][j]);
-    if (m === 1) {
-      a = 0;
-      b = 1;
-    } else if (n === 1) {
-      a = 1;
-      b = 0;
-    } else if (i < m - i && j < n - j) {
-      a = 0;
-      b = 1;
-    } else if (i < m - 1 && j === n - 1) {
-      a = 1;
-      b = 0;
-    } else if (i === m - 1 && j > 0) {
-      a = 0;
-      b = -1;
-    } else if (i < m && j === 0) {
-      a = -1;
-      b = 0;
+    if (i === y0 && j < x1) {
+      j += 1; // reach x1
+      if (j === x1) {
+        x1 -= 1;
+      }
+    } else if (n === 1 || (i < y1 && j === x1 + 1)) {
+      i += 1; // reach y1
+      if (i === y1) {
+        y1 -= 1;
+      }
+    } else if (i === y1 + 1 && x0 < j) {
+      j -= 1; // reach x0
+      if (j === x0) {
+        x0 += 1;
+        y0 += 1;
+      }
+    } else if (y0 < i && j === x0 - 1) {
+      i -= 1; // reach y0
     }
-    i += a;
-    j += b;
   }
   return result;
 };
+// j - x0 x1
+//
+//  i
+//  |
+// y0
+// y1
+//
 // @lc code=end
 
 module.exports = spiralOrder;
