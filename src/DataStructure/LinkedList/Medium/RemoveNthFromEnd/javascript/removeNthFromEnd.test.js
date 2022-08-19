b3c9a0b40fa996d3e947f26a1431f8e291865483
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-08-19 15:30:37 -0700

    refactor

--- src/DataStructure/LinkedList/Medium/RemoveNthFromEnd/javascript/removeNthFromEnd.test.js
+++ src/DataStructure/LinkedList/Medium/RemoveNthFromEnd/javascript/removeNthFromEnd.test.js
@@ -3,5 +3,5 @@ import runTestCase, { cases, toArray } from './testCases.js';
 
 test.each(cases.map(c => [c.name, c]))(
   '[Data Structure/Linked List/Medium/[19] Remove Nth Node From End of List] - Case %#',
-  (name, testCase) => expect(toArray(runTestCase(solution, testCase))).toEqual(testCase.expected)
+  (name, testCase) => expect(toArray(runTestCase(solution, testCase))).toEqual(testCase.expected),
 );
