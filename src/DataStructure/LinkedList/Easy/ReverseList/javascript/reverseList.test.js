a958ac0179e0c59f4d6523eff5bcbaceb4ab3047
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-08-21 17:07:29 -0700

    update test

--- src/DataStructure/LinkedList/Easy/ReverseList/javascript/reverseList.test.js
+++ src/DataStructure/LinkedList/Easy/ReverseList/javascript/reverseList.test.js
@@ -1,7 +1,13 @@
+import LinkedList from 'linked-list';
+
 import solution from './reverseList.es3.cjs';
 import runTestCase, { cases } from './testCases.js';
 
-test.skip.each(cases.map(c => [c.name, c]))(
+test.each(cases.map(c => [c.name, c]))(
   '[Data Structure/Linked List/Easy/[206] Reverse Linked List] - Case %#',
-  (name, testCase) => expect(runTestCase(solution, testCase)).toEqual(testCase.expected),
+  (name, testCase) => {
+    expect(
+      LinkedList.prototype.toArray.call({ head: runTestCase(solution, testCase) }),
+    ).toEqual(testCase.expected);
+  },
 );
