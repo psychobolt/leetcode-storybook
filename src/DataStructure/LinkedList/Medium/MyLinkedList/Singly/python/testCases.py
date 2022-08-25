0d2ba1b98cb8dc8ac9859f0f643a4f07a087126e
psychobolt <psychobolt@users.noreply.github.com>
2022-08-25 18:15:00 +0000

    deploy: 2cbc9c4080974e5315bb259e8947ffd1213f4343

---
+++ src/DataStructure/LinkedList/Medium/MyLinkedList/Singly/python/testCases.py
@@ -0,0 +1,19 @@
+import json
+
+try:
+    data = open('testCases.json')
+except FileNotFoundError:
+    from pathlib import Path
+    data = (Path(__file__).parent / '../../testCases.json').open()
+
+cases = json.load(data)
+
+
+def runTestCase(MyLinkedList, testCase):
+    list = MyLinkedList()
+    commands = testCase['input'][1:]
+    args = testCase['args'][1:]
+    return [None] + [
+        getattr(list, command)(*args[i])
+        for i, command in enumerate(commands)
+    ]
