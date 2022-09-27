533aeeb36d8f8d4bdb37c802cca8bda7efe981c6
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-09-27 15:45:52 -0700

    move problem to sub directory

--- src/DataStructure/Queue/Medium/MyCircularQueue/javascript/MyCircularQueue.test.js
+++ src/DataStructure/Queue/Medium/MyCircularQueue/javascript/MyCircularQueue.test.js
@@ -2,6 +2,6 @@ import solution from './MyCircularQueue.es3.cjs';
 import runTestCase, { cases } from './testCases.js';
 
 test.each(cases.map(c => [c.name, c]))(
-  '[Data Structure/Queue/[622] Design Circular Queue] - Case %#',
+  '[Data Structure/Queue/Medium/[622] Design Circular Queue] - Case %#',
   (name, testCase) => expect(runTestCase(solution, testCase)).toEqual(testCase.expected),
 );
