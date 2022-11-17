df822ddbfec26a86d8c9157dc7529cbb5bb52130
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-11-16 19:30:53 -0800

    revert change

--- src/DataStructure/Array/1D/Medium/FindMin/javascript/findMin.test.js
+++
@@ -1,7 +0,0 @@
-import solution from './findMin.es3.cjs';
-import runTestCase, { cases } from './testCases.js';
-
-test.each(cases.map(c => [c.name, c]))(
-  '[Data Structure/Array/1D/Medium/[153] Find Minimum in Rotated Sorted Array] - Case %#',
-  (name, testCase) => expect(runTestCase(solution, testCase)).toEqual(testCase.expected),
-);
