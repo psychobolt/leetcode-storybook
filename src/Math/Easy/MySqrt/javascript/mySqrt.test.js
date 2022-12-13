1a0590789d9ffa34ab00b2530ff8cd2c0a5aaa09
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-12-13 12:28:50 -0800

    refactor

--- src/Math/Easy/MySqrt/javascript/mySqrt.test.js
+++ src/Math/Easy/MySqrt/javascript/mySqrt.test.js
@@ -2,6 +2,6 @@ import solution from './mySqrt.es3.cjs';
 import runTestCase, { cases } from './testCases.js';
 
 test.each(cases.map(c => [c.name, c]))(
-  '[Algorithms/Binary Search/Easy/[69] Sqrt(x)] - Case %#',
+  '[Math/Easy/[69] Sqrt(x)] - Case %#',
   (name, testCase) => expect(runTestCase(solution, testCase)).toEqual(testCase.expected),
 );
