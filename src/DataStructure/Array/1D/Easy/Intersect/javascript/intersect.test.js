1bd910a18baa4e4ab3362aec3ffb8bb0d0d4e354
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-12-28 08:18:53 -0800

    refactor

--- src/DataStructure/Array/1D/Easy/Intersect/javascript/intersect.test.js
+++ src/DataStructure/Array/1D/Easy/Intersect/javascript/intersect.test.js
@@ -2,7 +2,7 @@ import solution from './intersect.js';
 import runTestCase, { cases } from './testCases.js';
 
 test.each(cases.map(c => [c.name, c]))(
-  '[Data Structure/Hash Map/Easy/[350] Intersection of Two Arrays II] - Case %#',
+  '[Data Structure/Array/1D/Easy/[350] Intersection of Two Arrays II] - Case %#',
   (name, testCase) => expect(runTestCase(solution, testCase).sort())
     .toEqual(testCase.expected.sort()),
 );
