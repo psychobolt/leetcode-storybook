ec0aa4db029e4e893c26fa0b55332ca8d80e4dc0
psychobolt <560721+psychobolt@users.noreply.github.com>
2023-01-22 16:34:50 -0800

    update solution 4

--- src/DataStructure/Array/1D/Hard/FindMedianSortedArrays/javascript/findMedianSortedArrays.es3.cjs
+++ src/DataStructure/Array/1D/Hard/FindMedianSortedArrays/javascript/findMedianSortedArrays.es3.cjs
@@ -5,33 +5,53 @@
  */
 
 // @lc code=start
+
+var INFINITY = 1000001;
+
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
  */
 var findMedianSortedArrays = function (nums1, nums2) {
-  var lastIndex1 = nums1.length - 1;
-  var lastIndex2 = nums2.length - 1;
+  var numsA = nums1;
+  var numsB = nums2;
+  var length = numsA.length + numsB.length;
+  var half = Math.trunc((numsA.length + numsB.length) / 2);
   var left = 0;
-  var right = lastIndex1 + lastIndex2 + 1;
-  var mid;
-  var current;
-  while (left + 1 < right) {
-    mid = left + Math.trunc((right - left) / 2);
-    if (mid > lastIndex1) {
-      mid -= nums1.length;
-      current = nums2[mid];
+  var leftA;
+  var leftB;
+  var right;
+  var rightA;
+  var rightB;
+  var midA;
+  var midB;
+  if (nums1.length > nums2.length) {
+    numsA = nums2;
+    numsB = nums1;
+  }
+  right = numsA.length - 1;
+  while (true) { // eslint-disable-line no-constant-condition
+    midA = Math.floor((left + right) / 2);
+    midB = half - midA - 2;
+
+    leftA = midA >= 0 ? numsA[midA] : -INFINITY;
+    rightA = midA + 1 < numsA.length ? numsA[midA + 1] : INFINITY;
+    leftB = midB >= 0 ? numsB[midB] : -INFINITY;
+    rightB = midB + 1 < numsB.length ? numsB[midB + 1] : INFINITY;
+
+    if (leftA <= rightB && leftB <= rightA) {
+      break;
+    } else if (leftA > rightB) {
+      right = midA - 1;
     } else {
-      current = nums1[mid];
-    }
-    if (nums1[left] > current && current < nums2[right - nums1.length]) {
-      left = mid + 1;
-    } else {
-      right = mid;
+      left = midA + 1;
     }
   }
-  return left; // ; right - nums1.length;
+  if (length % 2) {
+    return Math.min(rightA, rightB);
+  }
+  return (Math.max(leftA, leftB) + Math.min(rightA, rightB)) / 2.0;
 };
 // @lc code=end
 
