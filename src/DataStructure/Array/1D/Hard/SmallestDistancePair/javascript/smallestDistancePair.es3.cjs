1f9c3a06f9cda32aba8c2fa97ae4795755b29b23
psychobolt <560721+psychobolt@users.noreply.github.com>
2023-01-23 19:54:15 -0800

    add problem 719

---
+++ src/DataStructure/Array/1D/Hard/SmallestDistancePair/javascript/smallestDistancePair.es3.cjs
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=719 lang=javascript
+ *
+ * [719] Find K-th Smallest Pair Distance
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var smallestDistancePair = function(nums, k) {  // eslint-disable-line 
+
+};
+// @lc code=end
+
+module.exports = smallestDistancePair;
