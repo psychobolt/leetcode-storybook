3d523f11e071a4cc6eb0592402d60d57847f1c57
psychobolt <560721+psychobolt@users.noreply.github.com>
2023-01-24 20:05:00 -0800

    fix build

--- src/DataStructure/Array/1D/Hard/SmallestDistancePair/javascript/smallestDistancePair.test.js
+++ src/DataStructure/Array/1D/Hard/SmallestDistancePair/javascript/smallestDistancePair.test.js
@@ -1,7 +1,7 @@
 import solution from './smallestDistancePair.es3.cjs';
 import runTestCase, { cases } from './testCases.js';
 
-test.each.skip(cases.map(c => [c.name, c]))(
+test.skip.each(cases.map(c => [c.name, c]))(
   '[Data Structure/Array/1D/Hard/[719] Find K-th Smallest Pair Distance] - Case %#',
   (name, testCase) => expect(runTestCase(solution, testCase)).toEqual(testCase.expected),
 );
