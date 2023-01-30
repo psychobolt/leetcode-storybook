c6639b431ad069d5979f502612f646c235f00629
psychobolt <560721+psychobolt@users.noreply.github.com>
2023-01-30 13:16:23 -0800

    update solution

--- src/DataStructure/Array/1D/Hard/SmallestDistancePair/javascript/smallestDistancePair.es3.cjs
+++ src/DataStructure/Array/1D/Hard/SmallestDistancePair/javascript/smallestDistancePair.es3.cjs
@@ -10,8 +10,30 @@
  * @param {number} k
  * @return {number}
  */
-var smallestDistancePair = function(nums, k) {  // eslint-disable-line 
+var smallestDistancePair = function (nums, k) {
+  var lo;
+  var hi;
+  var mid;
 
+  var good = function (target) { // eslint-disable-line 
+    var count = 0;
+    return count >= k;
+  };
+
+  nums.sort();
+  lo = nums[0];
+  hi = nums[nums.length - 1];
+
+  while (lo < hi) {
+    mid = Math.floor((lo + hi) / 2);
+    if (good(mid)) {
+      hi = mid;
+    } else {
+      lo = mid + 1;
+    }
+  }
+
+  return lo;
 };
 // @lc code=end
 
